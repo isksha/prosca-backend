21d65d81917701f8418ae2e0e9502c265f429a85
Iskander Shangareev <iskander.shangareev@gmail.com>
2023-11-06 18:23:33 -0500

    added dao signatures

--- db/podsDb.js
+++ db/podsDb.js
@@ -4,6 +4,26 @@ const getPod = async (podId) => {
     return {1: 3};
 }
 
+const addCycle = async(pod_id, recurrence_rate, individual_constribution_rate) => {
+
+}
+
+const getCycleStatement = async(pod_id) => {
+
+}
+
+const addPod = async(pod_id, name, visibility, regulation, creator_id, creation_date) => {
+
+}
+
+const getAllPodMembers = async(pod_id) => {
+
+}
+
 module.exports = {
-    getPod
+    getPod,
+    addCycle,
+    getCycleStatement,
+    addPod,
+    getAllPodMembers
 };
--- db/usersDb.js
+++ db/usersDb.js
@@ -4,6 +4,16 @@ const getUser = async (podId) => {
     return {1: 2};
 }
 
+const getUserInfo = async (first_name, last_name, score) => {
+    // return User
+}
+
+const addUserInfo = async (email_address, score, phone_number, first_name, last_name, wallet_ammount, password, date_of_birth, ID) => {
+
+}
+
 module.exports = {
-    getUser
+    getUser,
+    getUserInfo,
+    addUserInfo
 };
