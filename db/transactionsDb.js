7b19031107067b0621551973e73e018d4233eb38
Iskander Shangareev <iskander.shangareev@gmail.com>
2023-11-06 18:23:33 -0500

    added dao signatures

--- db/transactionsDb.js
+++ db/transactionsDb.js
@@ -4,6 +4,11 @@ const getTransaction = async (podId) => {
     return {1: 2};
 }
 
+const consraddTransaction = async(Transaction_Id, amount, date, user_id, pod_id, pod_iteration) => {
+
+}
+
 module.exports = {
-    getTransaction
+    getTransaction,
+    consraddTransaction
 };
