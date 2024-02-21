7ea7e265ab3e577ede5fdbf87b14c8e0b96c4342
Iskander Shangareev <iskander.shangareev@gmail.com>
2024-02-20 22:02:23 -0500

    add post routes for deposits and withdrawals

--- routes/withdrawals.js
+++ routes/withdrawals.js
@@ -31,6 +31,20 @@ router.get('/blockchain/:transactionId', async (req, res) => {
 
 // ********************************    POST routes *********************************** //
 
+// curl -i -X POST -d 'user_id=cc18503e-3985-439d-a3a5-ce2da9e0c4e2&pod_id=c91461be-3315-4459-be5c-4211ece2b97a&contribution_amt=250' http://localhost:3000/withdrawals
+router.post('/', async (req, res) => {
+    const user = req.body.user_id
+    const pod = req.body.pod_id
+    const amt = req.body.contribution_amt
+
+    try {
+        const withdrawal = await dao.addWithdrawal(user, amt, pod);
+        res.status(200).json(withdrawal);
+    } catch (err) {
+        res.status(404).json({error: 'Withdrawal not posted'});
+    }
+});
+
 // ********************************     PUT routes *********************************** //
 
 // ********************************  DELETE routes *********************************** //
